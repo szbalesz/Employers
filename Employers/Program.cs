b430d297fb099eb466bf9b034492b82169dad590
SzBalesz <szarazb@kkszki.hu>
2024-09-17 11:42:18 +0200

    Fajl beolvasva

--- Employers/Program.cs
+++ Employers/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Employers
 {
@@ -10,7 +11,7 @@ namespace Employers
     {
         static void Main(string[] args)
         {
-
+            string[] fajl = File.ReadAllLines("tulajdonsagok_100sor.txt");
         }
     }
 }
